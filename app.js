a07298c1b6537b2ec97f6024244001c99f5f426c
손모함 <73145656+SonMoHam@users.noreply.github.com>
2021-06-30 16:35:08 +0900

    Create app.js

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+app.use(bodyParser.json());
+
+const todoRouter = require('./router');
+app.use(todoRouter);
+
+app.listen(3000);
